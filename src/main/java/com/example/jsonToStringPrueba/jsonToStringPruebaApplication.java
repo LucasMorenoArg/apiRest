a1c915775284db8b275f017a43f66137f4fe19ea
Lucas Moreno <lucas.moreno.ragni@mi.unc.edu.ar>
2025-08-28 21:34:48 -0300

    Refactor: renombrado de clases y limpieza de estructura

---
+++ src/main/java/com/example/jsonToStringPrueba/jsonToStringPruebaApplication.java
@@ -0,0 +1,14 @@
+package com.example.jsonToStringPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class jsonToStringPruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(jsonToStringPruebaApplication.class, args);
+        System.out.println("Hola Mundo jsonToStringPruebaApplication");}
+
+}
+
